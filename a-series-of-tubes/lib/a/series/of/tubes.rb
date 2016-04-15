cc32a47884cc6e0c957a8bfe8cdab5ce8b10d71d
Dan Phillips <danmakenoise@gmail.com>
2016-04-15 11:55:51 -0400

    initialize gem structure

---
+++ a-series-of-tubes/lib/a/series/of/tubes.rb
@@ -0,0 +1,11 @@
+require "a/series/of/tubes/version"
+
+module A
+  module Series
+    module Of
+      module Tubes
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ a-series-of-tubes/lib/a/series/of/tubes/version.rb
@@ -0,0 +1,9 @@
+module A
+  module Series
+    module Of
+      module Tubes
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
